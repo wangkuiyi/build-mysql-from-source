74cf8b616e1d9463a000cdeecdbf9286fd453f47
Yi Wang <yi.wang.2005@gmail.com>
2019-04-27 07:05:49 -0700

    Install pkg-config

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 # ubuntu:16.04 has too old GCC.
-FROM ubuntu:18.04 
+FROM ubuntu:18.04
 
 # https://dev.mysql.com/doc/mysql-sourcebuild-excerpt/5.5/en/source-installation.html
 RUN apt update
-RUN apt install -y build-essential cmake make gcc 
-RUN apt install -y libncurses5-dev 
+RUN apt install -y build-essential cmake make gcc
+RUN apt install -y libncurses5-dev
 RUN apt install -y bison m4
 RUN apt install -y git
 RUN apt install -y libnuma-dev
+RUN apt install -y pkg-config
 
 COPY build.sh /build.sh
 RUN chmod +x /build.sh
--- call-parser/main.cc
+++ call-parser/main.cc
@@ -2,34 +2,34 @@
 #include <string.h>
 
 #include "sql_class.h"
-#include "sql_digest.h"
-#include "sql_lex.h"
-#include "sql_parse.h"
-#include "table.h"
+// #include "sql_digest.h"
+// #include "sql_lex.h"
+// #include "sql_parse.h"
+// #include "table.h"
 
 int main() {
-  THD thd(true /*enable plugin*/);
-  const char *query_text = "select * from a_table";
-  uint query_length = strlen(query_text);
-  Object_creation_ctx ctx;
-  bool rc;
+  THD *thd = new THD;
+  // const char *query_text = "select * from a_table";
+  // uint query_length = strlen(query_text);
+  // Object_creation_ctx ctx;
+  // bool rc;
 
-  Parser_state parser_state;
-  if (parser_state.init(&thd, query_text, query_length)) {
-    printf("Cannot initialize parser state\n");
-  }
+  // Parser_state parser_state;
+  // if (parser_state.init(thd, query_text, query_length)) {
+  //   printf("Cannot initialize parser state\n");
+  // }
 
-  parser_state.m_input.m_compute_digest = true;
+  // parser_state.m_input.m_compute_digest = true;
 
-  rc = parse_sql(the, &parser_state, &ctx);
-  if (!rc) {
-    unsigned char md5[MD5_HASH_SIZE];
-    char digest_text[1024];
-    bool truncated;
-    const sql_digest_storage *digest = &thd.m_digest->m_digest_storage;
+  // rc = parse_sql(thd, &parser_state, &ctx);
+  // if (!rc) {
+  //   unsigned char md5[MD5_HASH_SIZE];
+  //   char digest_text[1024];
+  //   bool truncated;
+  //   const sql_digest_storage *digest = &thd->m_digest->m_digest_storage;
 
-    compute_digest_md5(digest, &md5[0]);
-    compute_digest_text(digest, &digest_text[0], sizeof(digest_text),
-                        &truncated);
-  }
+  //   compute_digest_md5(digest, &md5[0]);
+  //   compute_digest_text(digest, &digest_text[0], sizeof(digest_text),
+  //                       &truncated);
+  // }
 }
